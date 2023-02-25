ba722b1f2922d8ec5b7d2fc9b2785db1428bcef3
Tejas Desai <tejadesai0531@gmail.com>
2023-02-25 12:25:40 +0530

    changes

--- modules/route.js
+++ modules/route.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const countryRouter = require('./country/country.route')
+const cuisineRouter = require('./cuisine/cuisine.route')
 
 router.use('/country', countryRouter)
+router.use('/cuisine', cuisineRouter)
 
 module.exports = router;
